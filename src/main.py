0948da4ecfec218d805993a37686e9739db7c779
DanielKirshner <d.danielkirshner@gmail.com>
2024-12-09 00:01:51 +0200

    refactoring

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from Yad2CarFinder import Yad2CarFinder
 from CarSearchFilter import CarSearchFilter
 from Range import Range
+import json
 
 
+RESULTS_FILE_NAME = "results.json"
 yad2_car_finder = Yad2CarFinder()
 
 
@@ -11,4 +13,8 @@ if (__name__ == "__main__"):
         manufacturers=[CarSearchFilter.Manufacturer.KIA],
         models=[CarSearchFilter.Model.PICANTO],
         year_range=Range(2010))
-    yad2_car_finder.find(car_search_filter)
+    
+    result_urls = yad2_car_finder.find(car_search_filter)
+    
+    with open(RESULTS_FILE_NAME, "w") as f:
+        f.write(json.dumps(result_urls))
