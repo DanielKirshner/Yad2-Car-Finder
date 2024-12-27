6d594f9a17d304cba682413253dc300c4519b825
DanielKirshner <danielkirshner7878@gmail.com>
2024-12-27 17:16:19 +0200

    added cars link grabber module

--- src/main.py
+++ src/main.py
@@ -1,18 +1,12 @@
-from car_search_filter import CarSearchFilter
-from car_finder import Yad2CarFinder
-from config import Configuration
 from file_utils import FileUtils
-
-
-def retrieve_results_urls(car_search_filter: CarSearchFilter) -> list[str]:
-        yad2_car_finder = Yad2CarFinder()
-        return list(yad2_car_finder.find(car_search_filter))
+from config import Configuration
+from cars_link_retriever import CarsLinkRetriever
 
 
 def main():
     try:
         FileUtils.dump_to_file(
-             retrieve_results_urls(Configuration.CAR_SEARCH_FILTERS),
+             CarsLinkRetriever.retrieve_urls(Configuration.CAR_SEARCH_FILTERS),
              Configuration.RESULTS_FILE_PATH)
     except KeyboardInterrupt:
         print("Stopped by user!")
