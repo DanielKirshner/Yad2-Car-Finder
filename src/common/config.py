d04599a2f24963ec1711a56f33e60d86cab6baa7
DanielKirshner <d.danielkirshner@gmail.com>
2025-05-17 00:34:25 +0300

    Measure runtime

--- src/common/config.py
+++ src/common/config.py
@@ -9,8 +9,8 @@ class Configuration:
 
     CAR_SEARCH_FILTERS = CarSearchFilter(
             manufacturers=[CarSearchFilter.Manufacturer.KIA],
-            models=[CarSearchFilter.Model.PICANTO])
-            # year_range=Range(2024, 2025))
+            models=[CarSearchFilter.Model.PICANTO],
+            year_range=Range(2020, 2025))
     
     class Logger:
         DEFAULT_LOG_DIR_NAME: str = "logs"
