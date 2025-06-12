c2c6ce78086e349c2560a4285773f32856959138
Daniel Kirshner <d.danielkirshner@gmail.com>
2025-06-12 17:57:24 +0300

    Removed  timer

--- src/main.py
+++ src/main.py
@@ -3,22 +3,15 @@ from common.config import Configuration
 from common.logger import Logger
 from car.cars_link_retriever import CarsLinkRetriever
 import logging
-import time
 
 
 def main():
     try:
-        start_time = time.time()
-        
         Logger()
         
         FileUtils.dump_to_file(
              CarsLinkRetriever.retrieve_urls(Configuration.CAR_SEARCH_FILTERS),
-             Configuration.RESULTS_FILE_PATH)
-        
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        print(f"Execution time: {elapsed_time:.2f} seconds")
+             Configuration.RESULTS_FILE_PATH)        
     except KeyboardInterrupt:
         logging.warning("Stopped by user!")
 
